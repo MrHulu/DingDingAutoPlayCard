0ca69830e9de9eca762909ecdda315e259fbe9c6
MrHulu <491849417@qq.com>
2023-09-13 11:07:53 +0800

    other: 更新自动打卡策略

--- playCard.py
+++ playCard.py
@@ -1,12 +1,13 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
 import subprocess
 import time
 import setting
 import datetime
 import smtplib
 import os
-import random
+import time
 
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger 
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
@@ -24,7 +25,9 @@ screen_dir = setting.screen_dir
 # 打开钉钉以及关闭钉钉封装为一个妆饰器函数
 def with_open_close_dingding(func):
     def wrapper(self, *args, **kwargs):
-        print("打开钉钉")
+        t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        print(f"\n{t} 开始")
+        print('点亮屏幕，解锁，打开钉钉中...')
         operation_list = [self.adbpower, self.adbclear, self.adbopen_dingding]
         for operation in operation_list:
             process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
@@ -32,7 +35,7 @@ def with_open_close_dingding(func):
         # 确保完全启动，并且加载上相应按键（根据手机响应速度可以调整这里）
         time.sleep(8)
         
-        print("进入打卡页面")
+        print("打开打卡界面中...")
         operation_list1 = [self.adbselect_work, self.adbselect_playcard]
         for operation in operation_list1:
             process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
@@ -45,12 +48,13 @@ def with_open_close_dingding(func):
         # print("执行打卡操作")
         func(self, *args, **kwargs)
         
-        print("关闭钉钉")
+        print("返回桌面，退出钉钉，手机黑屏中...")
         operation_list2 = [self.adbback_index, self.adbkill_dingding, self.adbpower]
         for operation in operation_list2:
             process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
             process.wait()
-        print("kill dingding success")
+        t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        print(f"\n{t} 结束")
 
     return wrapper
 
@@ -72,7 +76,7 @@ class dingDing:
         self.adbselect_work = 'adb shell input tap %s' % (setting.work_position)
         # 点击考勤打卡
         self.adbselect_playcard = 'adb shell input tap %s' % (setting.card_position)
-        # 点击下班打卡
+        # 点击打卡
         self.adbclick_playcard = 'adb shell input tap %s' % (setting.play_position)
         # 设备截屏保存到sdcard
         self.adbscreencap = 'adb shell screencap -p sdcard/screen.png'
@@ -81,72 +85,38 @@ class dingDing:
         # 删除设备截屏
         self.adbrm_screencap = 'adb shell rm -r sdcard/screen.png'
 
-    # 点亮屏幕 》》解锁 》》打开钉钉
+    # 1 点亮屏幕 》》解锁 》》打开钉钉
     def open_dingding(self):
         operation_list = [self.adbpower, self.adbclear, self.adbopen_dingding]
-        print(f'open_dingding: {operation_list}')
+        print('点亮屏幕，解锁，打开钉钉中...')
         for operation in operation_list:
             process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
             process.wait()
         # 确保完全启动，并且加载上相应按键
         time.sleep(8)
-        print("open dingding success")
+        print("成功打开钉钉")
 
-    # 返回桌面 》》 退出钉钉 》》 手机黑屏
-    def close_dingding(self):
-        operation_list = [self.adbback_index, self.adbkill_dingding, self.adbpower]
-        for operation in operation_list:
-            process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
-            process.wait()
-        print("kill dingding success")
-
-    # 上班(极速打卡)
-    @with_open_close_dingding
-    def goto_work(self):
-        self.screencap()
-        self.send_email()
-        print("打卡成功")
-
-    # 打开打卡界面
+    # 2 打开打卡界面
     def openplaycard_interface(self):
-        print("打开打卡界面")
+        print("打开打卡界面中...")
         operation_list = [self.adbselect_work, self.adbselect_playcard]
         for operation in operation_list:
             process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
             process.wait()
             time.sleep(2)
         time.sleep(10)
-        print("open playcard success")
+        print("成功打开打卡界面")
 
-    # 手动点击打开
-    def click_playcard(self):
-        print("打卡")
-        process = subprocess.Popen([self.adbclick_playcard], shell=False, stdout=subprocess.PIPE)
-        process.wait()
-        time.sleep(2)
-        print("完成打卡")
-
-    # 下班
-    @with_open_close_dingding
-    def off_work(self):
-        operation_list = [self.adbclick_playcard]
-        for operation in operation_list:
-            process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
-            process.wait()
-            time.sleep(3)
-        self.screencap()
-        self.send_email()
-        print("afterwork playcard success")
-
-    # 截屏>> 发送到电脑 >> 删除手机中保存的截屏
+    # 3 截屏>> 发送到电脑 >> 删除手机中保存的截屏
     def screencap(self):
+        print("截屏, 发送至电脑中...")
         operation_list = [self.adbscreencap, self.adbpull, self.adbrm_screencap]
         for operation in operation_list:
             process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
             process.wait()
-        print("screencap to computer success")
+        print("成功发送至电脑")
 
-    # 发送邮件（QQ邮箱）
+    # 4 发送邮件（QQ邮箱）
     @staticmethod
     def send_email():
         """
@@ -177,31 +147,102 @@ class dingDing:
         except smtplib.SMTPException as e:
             print(e)
 
+    # 5 返回桌面 》》 退出钉钉 》》 手机黑屏
+    def close_dingding(self):
+        operation_list = [self.adbback_index, self.adbkill_dingding, self.adbpower]
+        print("返回桌面，退出钉钉，手机黑屏中...")
+        for operation in operation_list:
+            process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
+            process.wait()
+        print("成功关闭钉钉并锁屏")
 
-def job1():
-    print("开始打卡")
-    dingDing(directory).goto_work()
+    # 手动点击打开
+    def click_playcard(self):
+        print("打卡")
+        process = subprocess.Popen([self.adbclick_playcard], shell=False, stdout=subprocess.PIPE)
+        process.wait()
+        time.sleep(2)
+        print("完成打卡")
 
+    # 上班(极速打卡)
+    @with_open_close_dingding
+    def goto_work(self):
+        self.screencap()
+        self.send_email()
+        print("上班打卡成功")
 
-def job2():
-    print("开始打卡")
-    dingDing(directory).off_work()
+    # 下班
+    @with_open_close_dingding
+    def off_work(self):
+        operation_list = [self.adbclick_playcard]
+        for operation in operation_list:
+            process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
+            process.wait()
+            time.sleep(3)
+        self.screencap()
+        self.send_email()
+        print("下班打卡成功")
 
-def get_random_minute(min, max, num):
-    rnd = random.random()
-    if rnd > 0.8:
-        rand = random.randint(min, max)
+#####################################################################################################
+
+def auto_playCard(num):
+    scheduler = BlockingScheduler()
+
+    # 一周早上
+    trigger = CronTrigger(day_of_week='mon-fri', hour=9, minute=25, jitter=240)
+    scheduler.add_job(dingDing(directory).goto_work, trigger=trigger)
+
+    if num == 1:
+        # 全
+        trigger = CronTrigger(day_of_week='mon-fri', hour=21, minute=0, jitter=300)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
+    elif num == 2:
+        # [一五]21:00
+        # [二三]21:30
+        trigger = CronTrigger(day_of_week='mon,fri', hour=21, minute=0, jitter=300)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
+        trigger = CronTrigger(day_of_week='tue,wed', hour=21, minute=30, jitter=240)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
+    elif num == 3:
+        # [一二三]21:30
+        # [五]21:00
+        trigger = CronTrigger(day_of_week='mon,tue,wed', hour=21, minute=30, jitter=180)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
+        trigger = CronTrigger(day_of_week='fri', hour=21, minute=0, jitter=300)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
+    elif num == 4:
+        # [一二]21:30
+        # [四]22:00
+        trigger = CronTrigger(day_of_week='mon,tue', hour=21, minute=30, jitter=180)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
+        trigger = CronTrigger(day_of_week='thu', hour=22, minute=0, jitter=120)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
+    elif num == 5:
+        # [二三]21:00
+        # [四]22:30
+        trigger = CronTrigger(day_of_week='tue,wed', hour=21, minute=0, jitter=180)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
+        trigger = CronTrigger(day_of_week='thu', hour=22, minute=30, jitter=120)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
+    elif num == 6:
+        # [二四]21:30
+        trigger = CronTrigger(day_of_week='tue,thu', hour=21, minute=30, jitter=180)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
+    elif num == 7:
+        # [三]22:00
+        # [四]21:00
+        trigger = CronTrigger(day_of_week='wed', hour=22, minute=00, jitter=180)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
+        trigger = CronTrigger(day_of_week='thu', hour=21, minute=00, jitter=180)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
     else:
-        rand = random.randint(num, max)
-    return rand
+        trigger = CronTrigger(day_of_week='tue', hour=21, minute=30, jitter=180)
+        scheduler.add_job(dingDing(directory).off_work, trigger=trigger)
 
-# BlockingScheduler
-if __name__ == '__main__':
-    # scheduler = BlockingScheduler()
-    # scheduler.add_job(job1, 'cron', day_of_week='1-5', hour=go_hour, minute=get_random_minute(15,30,25))
-    # scheduler.add_job(job2, 'cron', day_of_week='1-5', hour=back_hour, minute=get_random_minute(0,10,5))
-    # scheduler.start()
-    print(directory)
+    scheduler.start()
+
+# 手动打卡，测试用，下班也生效的话，请在钉钉里设置下班也是极速打卡且每次打开钉钉会自动更新打卡
+def manually_playCard():
     d = dingDing(directory)
     d.open_dingding()
     d.openplaycard_interface()
@@ -209,3 +250,9 @@ if __name__ == '__main__':
     d.screencap()
     d.send_email()
     d.close_dingding()
+
+# BlockingScheduler
+if __name__ == '__main__':
+
+    auto_playCard(7)
+    # manually_playCard()
